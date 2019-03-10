c5f18ee8d21a3431f5219578d629570d6d28e7d7
unknown <mathewrogers@yahoo.com>
2019-03-10 16:30:15 -0700

    finished pipeling

--- cpu.v
+++ cpu.v
@@ -27,26 +27,30 @@ module cpu (
   assign debug_port3 = {4'b0, inst_w_addr};
 
  //control wires
-  wire reg_w_en;
-  wire load_inst;
-  wire store_inst;
+  wire reg_w_en_d, reg_w_en_x, reg_w_en_m, reg_w_en_w;
+  wire load_inst_d, load_inst_x, load_inst_m, load_inst_w;
+  wire store_inst_d, store_inst_x, store_inst_m;
   wire immediate_inst_d, immediate_inst_x;
   wire branch_inst_d, branch_inst_x;
   wire branch_link_d, branch_link_x, branch_link_m, branch_link_w;
   wire [3:0] opcode_d, opcode_x;
+  wire take_branch, take_branch_n, take_branch_n_n;
+  wire squash;
   ///////////////
 
   //connection wires/////
   reg [31:0] instruction_f, instruction_d, instruction_x, instruction_m, instruction_w;
   reg [31:0] pc, pc_d, pc_x, pc_m, pc_n, pc_w;
-  reg [31:0] read_reg_one_d, read_reg_one_x, read_reg_two_d, read_reg_two_x, read_reg_two_m;
+  reg [31:0] read_reg_one_d, read_reg_one_x, read_reg_two_d, read_reg_two_x;
+  reg [31:0] reg_or_forward_x, reg_or_forward_m;
   reg [31:0] ALU_o_x, ALU_o_m, ALU_o_w;
   reg [31:0] read_data_m, read_data_w;
   reg [31:0] write_reg;
-  reg [31:0] immediate;
-  reg [31:0] alu_two_i;
+  reg [31:0] immediate_d, immediate_x;
+  reg [31:0] ALU_one_i, ALU_two_i;
   reg [31:0] data_into_reg;
-  reg [3:0]  inst_w_addr;
+  reg [3:0]  inst_write_addr;
+  reg [1:0]  choice_one, choice_two;
   reg        overflow;
   reg        carry;
   ///////////////////////
@@ -58,27 +62,31 @@ module cpu (
   control_unit main_control
     (.instruction(instruction_d[31:20])
     ,.resetn(resetn)
-    ,.reg_w_en(reg_w_en)
-    ,.load_inst(load_inst)
-    ,.store_inst(store_inst)
+    ,.reg_w_en(reg_w_en_d)
+    ,.load_inst(load_inst_d)
+    ,.store_inst(store_inst_d)
     ,.immediate_inst(immediate_inst_d)
     ,.branch_inst(branch_inst_d)
-    ,.opcode(opcode)
-    ,.branch_link(branch_link)
+    ,.opcode(opcode_d)
+    ,.branch_link(branch_link_d)
     );
 
   branch_control_unit branch_check
-    (.zero_r(zero_r)
+    (.instruction(instruction_x[31:28])
+    ,.zero_r(zero_r)
     ,.negative_r(negative_r)
     ,.carry_r(carry_r)
     ,.overflow_r(overflow_r)
     ,.pass_cond(pass_cond)
     );
 
+  assign take_branch = branch_inst_x & pass_cond;
+  assign squash = ((take_branch | take_branch_n) & take_branch_n_n);
+
   mux32b2to1 pc_add
     (.zero_i(pc + 4)
-    ,.one_i(pc_x + {{6{instruction_m[23]}}, instruction_m[23:0], 2'b00})
-    ,.select_i(branch_inst_x & pass_cond)
+    ,.one_i(pc_x + {{6{instruction_x[23]}}, instruction_x[23:0], 2'b00})
+    ,.select_i(take_branch)
     ,.out_o(pc_n)
     );
 
@@ -88,16 +96,16 @@ module cpu (
     );
 
   mux4b2to1 write_reg_mux
-    (.zero_i(instruction_x[15:12])
+    (.zero_i(instruction_w[15:12])
     ,.one_i(4'b1110)
-    ,.select_i(branch_link)
+    ,.select_i(branch_link_w)
     ,.out_o(inst_write_addr)
     );
 
   registers arm_regs
     (.rd_addr_1_i(instruction_d[19:16])
     ,.rd_addr_2_i(instruction_d[3:0])
-    ,.w_addr_i(inst_write_addr_w)
+    ,.w_addr_i(inst_write_addr)
     ,.data_i(write_reg)
     ,.w_en_i(reg_w_en_w)
     ,.clk_i(clk)
@@ -112,19 +120,36 @@ module cpu (
     ,.out_o(immediate_d)
     );
 
+  forwarding_unit data_one_mux
+    (.destination(instruction_m[15:12])
+    ,.source(instruction_x[19:16])
+    ,.load(load_inst_m)
+    ,.store(store_inst_m)
+    ,.choice(choice_one)
+    );
+
+  forwarding_unit data_two_mux
+    (.destination(instruction_m[15:12])
+    ,.source(instruction[3:0])
+    ,.load(load_inst_m)
+    ,.store(store_inst_m)
+    ,.choice(choice_two)
+    );
+
+
   mux32b3to1 forwarding_one
     (.zero_i(read_reg_one_x)
     ,.one_i(ALU_o_m)
-    ,.two_i(write_reg)
-    ,.select_i()
+    ,.two_i(read_data_m)
+    ,.select_i(choice_one)
     ,.out_o(ALU_one_i)
     );
 
   mux32b3to1 forwarding_two
     (.zero_i(read_reg_two_x)
     ,.one_i(ALU_o_m)
-    ,.two_i(write_reg)
-    ,.select_i()
+    ,.two_i(read_data_m)
+    ,.select_i(choice_two)
     ,.out_o(reg_or_forward_x)
     );
 
@@ -167,12 +192,18 @@ module cpu (
     ,.out_o(write_reg)
     );
 
+
+  //pc gen
   always @(posedge clk) begin
     if (~resetn) begin
       pc <= 32'b0;
     end else begin
       pc <= pc_n;
     end
+  end
+
+  //condition bits register
+  always @(posedge clk) begin
     if ((~branch_inst) & (~store_inst) & (~load_inst)) begin
       negative_r <= ALU_o_x[31];
       zero_r <= (ALU_o_x == 32'b0);
@@ -194,54 +225,65 @@ module cpu (
 
   //decode to execute
   always @(posedge clk) begin
-    if (~resetn) begin
+    if (squash) begin
       instruction_x <= 32'b0;
       pc_x <= 32'b0;
       read_reg_one_x <= 32'b0;
       read_reg_two_x <= 32'b0;
       opcode_x <= 4'b0;
       immediate_x <= 32'b0;
+      load_inst_x <= 1'b0;
+      store_inst_x <= 1'b0;
+      reg_w_en_x <= 1'b0;
+      branch_link_x <= 1'b0;
+      immediate_inst_x <= 1'b0;
+      branch_inst_x <= 1'b0;
     end else begin
       instruction_x <= instruction_d;
       pc_x <= pc_d;
       read_reg_one_x <= read_reg_one_d;
       read_reg_two_x <= read_reg_two_d;
-      opcode_x <= opcode;
+      opcode_x <= opcode_d;
       immediate_x <= immediate_d;
+      load_inst_x <= load_inst_d;
+      store_inst_x <= store_inst_d;
+      reg_w_en_x <= reg_w_en_d;
+      branch_link_x <= branch_link_d;
+      immediate_inst_x <= immediate_inst_d;
+      branch_inst_x <= branch_inst_d;
     end
   end
-  
+
   //execute to memory
   always @(posedge clk) begin
-    if (~resetn) begin
-      instruction_m <= 32'b0;
-      read_reg_two_m <= 32'b0;
-      ALU_o_m <= 32'b0;
-      pc_m <= 32'b0;
-    end else begin
-      instruction_m <= instruction_x;
-      reg_or_forwarded_m <= reg_or_forwarded_x;
-      ALU_o_m <= ALU_o_x;
-      pc_m <= pc_x;
-    end
+    instruction_m <= instruction_x;
+    reg_or_forward_m <= reg_or_forward_x;
+    ALU_o_m <= ALU_o_x;
+    pc_m <= pc_x;
+    load_inst_m <= load_inst_x;
+    store_inst_m <= store_inst_x;
+    reg_w_en_m <= reg_w_en_x;
+    branch_link_m <= branch_link_x;
+    take_branch_m <= take_branch_x;
   end
-  
+
   //memory to write
   always @(posedge clk) begin
-    if (~resetn) begin
-      read_data_w <= 32'b0;
-      ALU_o_w <= 32'b0;
-      instruction_w <= 32'b0;
-      pc_w <= 32'b0;
-    end else begin
-      read_data_w <= read_data_m;
-      ALU_o_w <= ALU_o_m;
-      instruction_w <= instruction_m;
-      pc_w <= pc_m;
-    end
+    read_data_w <= read_data_m;
+    ALU_o_w <= ALU_o_m;
+    instruction_w <= instruction_m;
+    pc_w <= pc_m;
+    load_inst_w <= load_inst_m;
+    reg_w_en_w <= reg_w_en_m;
+    branch_link_w <= branch_link_m;
+  end
+
+  //squash instructions
+  always @(posedge clk) begin
+    take_branch_n <= take_branch;
+    take_branch_n_n <= take_branch_n;
   end
 endmodule
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////helper modules//////////////////////////////////////////////////////////
@@ -448,15 +490,15 @@ module control_unit
   always @(*) begin
     if ((~branch_inst) & (~store_inst) & (~load_inst)) begin
       opcode = instruction[4:1];
-    end else
+    end else begin
       opcode = 4'b0100;
     end
   end
-
 endmodule
 
 module branch_control_unit
-  ( input wire zero_r
+  ( input wire [3:0] instruction
+  , input wire zero_r
   , input wire negative_r
   , input wire carry_r
   , input wire overflow_r
@@ -464,7 +506,7 @@ module branch_control_unit
   );
 
   always @(*) begin
-    case (instruction[11:8])
+    case (instruction)
       4'b0000 : pass_cond = zero_r;
       4'b0001 : pass_cond = ~zero_r;
       4'b0010 : pass_cond = carry_r;
@@ -486,13 +528,12 @@ module branch_control_unit
   end
 endmodule
 
-module pipeline_control
-  ( input wire [3:0] Rd
-  , input wire [3:0] Rn
-  , input wire [3:0] Rm
-  , input wire Immediate
-  , output wire mux_en
+module forwarding_unit
+  ( input wire [3:0] destination
+  , input wire [3:0] source
+  , input wire load
+  , input wire store
+  , output reg [1:0] choice
   );
-
-  assign mux_en = ((Rd == Rn) | ((Rd == Rm) & ~Immediate));
+  assign choice = ((destination !== source) | store) ? 2'b00 : load ? 2'b10 : 2'b01;
 endmodule
